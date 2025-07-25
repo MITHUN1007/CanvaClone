a30107943f43852e15e8ec4103933b82364fe647
MITHUN1007 <kirishkirish2k12@gmail.com>
2025-07-25 11:14:57 +0530

    Remove runtime configuration from middleware

--- middleware.ts
+++ middleware.ts
@@ -17,5 +17,4 @@ export const config = {
   // The following matcher runs middleware on all routes
   // except static assets.
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-  runtime: 'nodejs',
 };
